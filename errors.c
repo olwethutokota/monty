599e096d158019609925c26c8fcf53d39e64f10a
olwethutokota <olwethutokota13@gmail.com>
2023-11-16 08:21:49 -0800

    Monty

---
+++ errors.c
@@ -0,0 +1,116 @@
+#include "monty.h"
+
+/**
+ * err - Prints appropiate error messages that are determined by their error code.
+ * @error_code: Here are the error codes are the following:
+ * (1) => User doesnt give any file or more than one file to program.
+ * (2) => File provided isnt a file that can be opened or read.
+ * (3) => File provided contains an invalid instruction.
+ * (4) => When program is unable to malloc more memory.
+ * (5) => When parameter passed to the instruction "push" isnt an int.
+ * (6) => When stack it empty for pint.
+ * (7) => When stack it empty for pop.
+ * (8) => When stack is too short for operation.
+ */
+void err(int error_code, ...)
+{
+	va_list ag;
+	char *op;
+	int l_num;
+
+	va_start(ag, error_code);
+	switch (error_code)
+	{
+		case 1:
+			fprintf(stderr, "USAGE: monty file\n");
+			break;
+		case 2:
+			fprintf(stderr, "Error: Can't open file %s\n",
+				va_arg(ag, char *));
+			break;
+		case 3:
+			l_num = va_arg(ag, int);
+			op = va_arg(ag, char *);
+			fprintf(stderr, "L%d: unknown instruction %s\n", l_num, op);
+			break;
+		case 4:
+			fprintf(stderr, "Error: malloc failed\n");
+			break;
+		case 5:
+			fprintf(stderr, "L%d: usage: push integer\n", va_arg(ag, int));
+			break;
+		default:
+			break;
+	}
+	free_nodes();
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * more_err - Will handles errors.
+ * @error_code: Error codes are following:
+ * (6) => When stack it empty for pint.
+ * (7) => When stack it empty for pop.
+ * (8) => When stack is too short for operation.
+ * (9) => Division by zero.
+ */
+void more_err(int error_code, ...)
+{
+	va_list ag;
+	char *op;
+	int l_num;
+
+	va_start(ag, error_code);
+	switch (error_code)
+	{
+		case 6:
+			fprintf(stderr, "L%d: can't pint, stack empty\n",
+				va_arg(ag, int));
+			break;
+		case 7:
+			fprintf(stderr, "L%d: can't pop an empty stack\n",
+				va_arg(ag, int));
+			break;
+		case 8:
+			l_num = va_arg(ag, unsigned int);
+			op = va_arg(ag, char *);
+			fprintf(stderr, "L%d: can't %s, stack too short\n", l_num, op);
+			break;
+		case 9:
+			fprintf(stderr, "L%d: division by zero\n",
+				va_arg(ag, unsigned int));
+			break;
+		default:
+			break;
+	}
+	free_nodes();
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * string_err - handles errors.
+ * @error_code: Error codes are the following:
+ * (10) ~> Number inside a node is outside ASCII bounds.
+ * (11) ~> Stack is empty.
+ */
+void string_err(int error_code, ...)
+{
+	va_list ag;
+	int l_num;
+
+	va_start(ag, error_code);
+	l_num = va_arg(ag, int);
+	switch (error_code)
+	{
+		case 10:
+			fprintf(stderr, "L%d: can't pchar, value out of range\n", l_num);
+			break;
+		case 11:
+			fprintf(stderr, "L%d: can't pchar, stack empty\n", l_num);
+			break;
+		default:
+			break;
+	}
+	free_nodes();
+	exit(EXIT_FAILURE);
+}
---
+++ main.c
@@ -0,0 +1,83 @@
+#include "monty.h"
+stack_t *head = NULL;
+
+/**
+ * main - .....
+ * @argc: ....
+ * @argv: ....
+ * Return: ...
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	open_file(argv[1]);
+	free_nodes();
+	return (0);
+}
+
+/**
+ * create_node - .....
+ * @n: ....
+ * Return: ...
+ */
+stack_t *create_node(int n)
+{
+	stack_t *node;
+
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+		err(4);
+	node->next = NULL;
+	node->prev = NULL;
+	node->n = n;
+	return (node);
+}
+
+/**
+ * free_nodes - ....
+ */
+void free_nodes(void)
+{
+	stack_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
+
+
+/**
+ * add_to_queue - ...
+ * @new_node: ...
+ * @ln: ...
+ */
+void add_to_queue(stack_t **new_node, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
+
+	if (new_node == NULL || *new_node == NULL)
+		exit(EXIT_FAILURE);
+	if (head == NULL)
+	{
+		head = *new_node;
+		return;
+	}
+	tmp = head;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = *new_node;
+	(*new_node)->prev = tmp;
+
+}
